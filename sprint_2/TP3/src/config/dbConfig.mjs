778da0929f426a5528916e08795825a44ff3aaeb
barrfigueroa021 <barrbarafigueroa021@gmail.com>
2024-11-05 11:16:27 -0300

    ..

--- sprint_2/TP3/src/config/dbConfig.mjs
+++ sprint_2/TP3/src/config/dbConfig.mjs
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
-
 export async function connectDB() {
-  mongoose.connect("mongodb+srv://Grupo-11:grupo11@cursadanodejs.ls9ii.mongodb.net/Node-js",
-    {
-      useNewUrlParser:true,
-      useUnifiedTopology:true
-    }
-  )
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Grupo-11:grupo11@cursadanodejs.ls9ii.mongodb.net/Node-js",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  }
 }
